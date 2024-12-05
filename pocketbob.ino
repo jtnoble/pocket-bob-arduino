34fc2ed9f08479e88c9bb0414e861ae921dd2753
Joe <104746837+jtnoble@users.noreply.github.com>
2024-12-04 19:49:09 -0600

    Adafruit -> LCDWIKI pocketbob.ino
    Needed to change this to work with the model of ILI9341 I received.

--- pocketbob.ino
+++ pocketbob.ino
@@ -5,11 +5,19 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include <LCDWIKI_GUI.h> //Core graphics library
+#include <LCDWIKI_SPI.h> //Hardware-specific library
 
 // TFT Defines
-#define TFT_DC 9
-#define TFT_CS 10
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+#define MODEL ILI9341
+#define TFT_LED A0
+#define TFT_RST A4
+#define TFT_DC A3
+#define TFT_CS A5
+#define TFT_MOSI 11
+#define TFT_MISO 12
+#define TFT_SCK 13
+LCDWIKI_SPI tft = LCDWIKI_SPI(MODEL, TFT_CS, TFT_DC, TFT_MISO, TFT_MOSI, TFT_RST, TFT_SCK, TFT_LED);
 
 // Button Defines
 #define UP_BTN 7
@@ -56,11 +64,11 @@ StateFunction states[] = {start_screen, selection_menu, random_quote_menu, brows
 
 // Utility Functions
 void clear_screen() {
-    tft.fillScreen(ILI9341_BLACK);
+    tft.Fill_Screen(ILI9341_BLACK);
 }
 
 void center_cursor() {
-    tft.setCursor(100, 100);
+    // tft.setCursor(100, 100);
 }
 
 void change_state(int next_state) {
@@ -107,59 +115,61 @@ void b_press() {
 
 // Core Menu Screens
 void start_screen() {
+    Serial.println("Starting");
     clear_screen();
-    tft.setCursor(40, 20);
-    tft.setTextColor(ILI9341_GREEN);
-    tft.setTextSize(2);
-    tft.println("####################");
+    // tft.setCursor(40, 20);
+    tft.Set_Text_colour(ILI9341_GREEN);
+    tft.Set_Text_Size(2);
+    tft.Print_String("####################", 40, 0);
     delay(500);
-    tft.setCursor(40, 50);
-    tft.println("###  POCKET BOB  ###");
+    // tft.setCursor(40, 50);
+    tft.Print_String("###  POCKET BOB  ###", 40, 20);
     delay(500);
-    tft.setCursor(40, 80);
-    tft.println("####################");
+    // tft.setCursor(40, 80);
+    tft.Print_String("####################", 40, 40);
     delay(500);
-    tft.setCursor(40, 180);
-    tft.println("Press A To Continue.");
+    // tft.setCursor(40, 180);
+    tft.Print_String("Press A To Continue.", 40, 120);
 }
 
 void selection_menu() {
     clear_screen();
-    tft.setCursor(0, 0);
-    tft.println("Menu:");
+    // tft.setCursor(0, 0);
+    tft.Print_String("Menu:\n", 0, 0);
     for (int i = 0; i < menu_len; i++) {
         if (i == selection_menu_curr_selection) {
-            tft.print("> ");
+            tft.Print_String("> " + (String)menu_options[i], 0, i*40+40);
         } else {
-            tft.print("  ");
+            tft.Print_String("  " + (String)menu_options[i], 0, i*40+40);
         }
-        tft.println(menu_options[i]);
     }
+
+    
 }
 
 void random_quote_menu() {
     clear_screen();
     randomSeed(millis());
     int rand = random(quotes_len);
-    tft.setCursor(0, 0);
-    tft.println("Random Quote:");
-    tft.println(quotes[rand]);
+    // tft.setCursor(0, 0);
+    tft.Print_String("Random Quote:", 0, 0);
+    tft.Print_String(quotes[rand], 0, 40);
 }
 
 void browse_quotes_menu() {
     clear_screen();
-    tft.setCursor(0, 0);
-    tft.print(quotes_menu_curr_selection + 1);
-    tft.print("/");
-    tft.print(quotes_len);
-    tft.print(": ");
-    tft.println(quotes[quotes_menu_curr_selection]);
+    // tft.setCursor(0, 0);
+    tft.Print_String((String)quotes_menu_curr_selection + 1, 0, 0);
+    tft.Print_String("/", 10, 0);
+    tft.Print_String((String)quotes_len, 20, 0);
+    tft.Print_String(": ", 30, 0);
+    tft.Print_String(quotes[quotes_menu_curr_selection], 0, 40);
 }
 
 void credits_menu() {
     clear_screen();
-    tft.setCursor(0, 0);
-    tft.println("Made by Jtnoble\n\nPress B to go back.");
+    // tft.setCursor(0, 0);
+    tft.Print_String("Made by Jtnoble\n\nPress B to go back.", 0, 0);
 }
 
 // Handle Button Presses
@@ -195,8 +205,8 @@ void checkButtonStates() {
 
 // Setup and Main Loop
 void setup() {
-    tft.begin();
-    tft.setRotation(3);
+    tft.Init_LCD();
+    tft.Set_Rotation(3);
     for (int i = 0; i < NUM_BUTTONS; i++) {
         pinMode(buttonPins[i], INPUT);
     }
